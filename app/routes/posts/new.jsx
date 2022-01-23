4359663b2c82e042cf0278ebd54d0164620a1df5
Roman Kolivashko <rkolivashko@gmail.com>
2022-01-22 21:18:08 -0800

    Added server-side form validation metods

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,5 +1,18 @@
-import { Link, redirect } from "remix";
+//import { json } from "build";
+import { Link, redirect, useActionData, json } from "remix";
 import { db } from "~/utils/db.server";
+
+function validateTitle(title) {
+  if (typeof title !== "string" || title.length < 3) {
+    return "Title should be at least 3 characters long";
+  }
+}
+
+function validateBody(body) {
+  if (typeof body !== "string" || body.length < 10) {
+    return "Post body should be at least 10 characters long";
+  }
+}
 
 export const action = async ({ request }) => {
   const form = await request.formData();
@@ -8,6 +21,16 @@ export const action = async ({ request }) => {
 
   const fields = { title, body };
 
+  const fieldErrors = {
+    title: validateTitle(title),
+    body: validateBody(body),
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    console.log(fieldErrors);
+    return json({ fieldErrors, fields }, { status: 400 });
+  }
+
   //submit to database
   const post = await db.post.create({ data: fields });
 
@@ -15,6 +38,8 @@ export const action = async ({ request }) => {
 };
 
 function NewPost() {
+  const actionData = useActionData();
+
   return (
     <>
       <div className="page-header">
@@ -28,11 +53,31 @@ function NewPost() {
         <form method="POST">
           <div className="form-control">
             <label htmlFor="title">Title</label>
-            <input type="text" name="title" id="title" />
+            <input
+              type="text"
+              name="title"
+              id="title"
+              defaultValue={actionData?.fields?.title}
+            />
+            <div className="error">
+              <p>
+                {actionData?.fieldErrors?.title &&
+                  actionData?.fieldErrors?.title}
+              </p>
+            </div>
           </div>
           <div className="form-control">
             <label htmlFor="body">Post Body</label>
-            <textarea name="body" id="body" />
+            <textarea
+              name="body"
+              id="body"
+              defaultValue={actionData?.fields?.body}
+            />
+            <div className="error">
+              <p>
+                {actionData?.fieldErrors?.body && actionData?.fieldErrors?.body}
+              </p>
+            </div>
           </div>
           <button type="submit" className="btn btn block">
             Add Post
